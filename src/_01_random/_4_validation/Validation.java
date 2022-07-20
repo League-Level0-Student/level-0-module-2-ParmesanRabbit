8b51fa50675dffd145dcb1093e649bda80555646
root <root@a257f7b544d6>
2022-07-20 23:51:02 +0000

    finished module 1 yay

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -4,9 +4,7 @@
 package _01_random._4_validation;
 
 import java.util.Random;
-
 import javax.swing.JOptionPane;
-
 public class Validation {
 	public static void main(String[] args) {
 		
@@ -17,7 +15,9 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber ==1) {
+			
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
